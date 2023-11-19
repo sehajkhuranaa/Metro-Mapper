8a2494d30f26a0b0d442be0cd5afa731f8734e19
sehajkhuranaa <128689947+sehajkhuranaa@users.noreply.github.com>
2023-11-19 19:09:26 +0000

    ok

--- card_operations.h
+++
@@ -1,6 +0,0 @@
-#include <string> // Added this line to include the string header file
-#ifndef CARD_OPERATIONS_H
-#define CARD_OPERATIONS_H
-void recharge(std::string username); // Changed string to std::string
-
-#endif
--- graph_operations.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef GRAPH_OPERATIONS_H
-#define GRAPH_OPERATIONS_H
-
-void loadGraph1();
-void loadGraph2();
-
-#endif
